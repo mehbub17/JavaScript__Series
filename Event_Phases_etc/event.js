36ddca3794bf4b97f95eafd418242aa9f86b00c1
mehbub17 <mehbubhussain.17@gmail.com>
2025-02-14 18:42:42 +0530

    bubbling_1

--- Event_Phases_etc/event.js
+++ Event_Phases_etc/event.js
@@ -6,5 +6,10 @@
 var div  = document.querySelector("body");
 
 div.addEventListener("click",()=>{
-    console.log("div");
+    console.log("now this will be called after it child gets called");
+})
+
+var button = document.querySelector("button");
+button.addEventListener("click",()=>{
+    console.log("Button clicked-->this will be called first as event propagates from child to parent __also known as event bubbling");
 })
