d4b4a2e8ab5db0fa61da40d045c0cc2c51bd731c
mehbub17 <mehbubhussain.17@gmail.com>
2025-02-14 18:49:15 +0530

    event capturing

--- Event_Phases_etc/event.js
+++ Event_Phases_etc/event.js
@@ -13,3 +13,18 @@ var button = document.querySelector("button");
 button.addEventListener("click",()=>{
     console.log("Button clicked-->this will be called first as event propagates from child to parent __also known as event bubbling");
 })
+
+
+
+//capturing--->> just give true after callback
+
+var div1 = document.querySelector("#div_1");
+var btn1 = document.querySelector("#btn_1");
+
+div1.addEventListener('click',()=>{
+    console.log("Parent will be called first as event capturing is done here");
+},true);
+
+btn1.addEventListener("click",()=>{
+    console.log("Child tag will be called after parent as event capturing is done for this block");
+},true);
