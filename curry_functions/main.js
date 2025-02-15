37082ccc23b9ef6c67c3df9e1a7468b5aedc9a16
mehbub17 <mehbubhussain.17@gmail.com>
2025-02-15 06:53:00 +0530

    curry for web pages dynamic text change

--- curry_functions/main.js
+++ curry_functions/main.js
@@ -1,12 +1,5 @@
 
-// partial currying
-const curry_mul = x=>y=>x*y;
-const curry_add = x=>y=>x+y;
-const curry_sub = x=>y=>x-y;
 
-const mul5 = curry_mul(5);
-const add5 = curry_add(5);
+//curry example for web pages
 
-console.log(mul5(3)); // 15
-
-console.log(add5(mul5(3))); // 20
+const updateElementText = id => text => document.querySelector(`${id}`).textContent = text;
