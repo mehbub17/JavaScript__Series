2d88733784be3618a8acaba7871f3e07779eed5a
mehbub17 <mehbubhussain.17@gmail.com>
2025-02-15 06:50:24 +0530

    partial currying

--- curry_functions/main.js
+++ curry_functions/main.js
@@ -1,10 +1,12 @@
 
-
-
-const mul = (x,y)=>x*y;
-
+// partial currying
 const curry_mul = x=>y=>x*y;
+const curry_add = x=>y=>x+y;
+const curry_sub = x=>y=>x-y;
 
+const mul5 = curry_mul(5);
+const add5 = curry_add(5);
 
-console.log(mul(2,3));
-console.log(curry_mul(2)(3));
+console.log(mul5(3)); // 15
+
+console.log(add5(mul5(3))); // 20
