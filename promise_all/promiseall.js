225e634507751a5cd9cfcb18b61b98a540f47da3
mehbub17 <mehbubhussain.17@gmail.com>
2025-02-15 06:32:26 +0530

    case-2 if all the promise is resolved then only promise.all runs then otherwise catch

--- promise_all/promiseall.js
+++ promise_all/promiseall.js
@@ -5,47 +5,77 @@
 //resolve resolve resolve
 
 
-const fun_1 = ()=>{
+// const fun_1 = ()=>{
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//             resolve("function one is resolved and data is returned");
+//         },5000);
+//     });
+// }
+
+
+// const fun_2 = ()=>{
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//             const data = [1,2,3,4];
+//             resolve(data);
+//         },3000);
+//     });
+// }
+// const fun_3 = ()=>{
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//             resolve("function three is resolved and data is returned");
+//         },3000);
+//     });
+// }
+
+
+// Promise.all([
+//     fun_1(),
+//     fun_2(),
+//     fun_3()
+// ])
+// .then((arr)=>{
+//     console.log("All are resolved and all data are ");
+//     console.log(arr);
+// })
+// .catch(error=>console.log("Error in promise",error))
+// .finally(()=>console.log("All Done"));
+
+
+// const data = await fun_2();
+
+// data.forEach(element => {
+//     console.log(element*2);
+// });
+
+
+//case 2  resolve rejected
+
+const getData = ()=>{
     return new Promise((resolve,reject)=>{
         setTimeout(()=>{
-            resolve("function one is resolved and data is returned");
-        },5000);
+            resolve("Data from gatData one");
+        },2000);
     });
 }
 
-
-const fun_2 = ()=>{
+const getError = ()=>{
     return new Promise((resolve,reject)=>{
         setTimeout(()=>{
-            const data = [1,2,3,4];
-            resolve(data);
-        },3000);
-    });
-}
-const fun_3 = ()=>{
-    return new Promise((resolve,reject)=>{
-        setTimeout(()=>{
-            resolve("function three is resolved and data is returned");
-        },3000);
+            reject("Data not available error here");
+        },2000);
     });
 }
 
 
 Promise.all([
-    fun_1(),
-    fun_2(),
-    fun_3()
-])
-.then((arr)=>{
-    console.log("All are resolved and all data are ");
-    console.log(arr);
-})
-.catch(error=>console.log("Error in promise",error))
-.finally(()=>console.log("All Done"));
-
-
-const data = await fun_2();
-
-data.forEach(element => {
-    console.log(element*2);
-});
+    getData(),
+    getError()])
+    .then(dataArray=>{
+        console.log("All data fetched",dataArray);
+    })
+    .catch(error=>{
+        console.log("Error occurred ", error);
+    });
