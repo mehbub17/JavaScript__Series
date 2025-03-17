75c4d6999e78190fd7430cf099009421f6fed01d
mehbub17 <mehbubhussain.17@gmail.com>
2025-03-17 05:37:20 +0530

    mutable vs immutable vs reassignment

--- shallow & deep copy/main.js
+++ shallow & deep copy/main.js
@@ -17,3 +17,24 @@ console.log(arr);
 console.log(brr);
 brr[2] = 22; // changes the original arr as brr is referencing arr
 console.log(arr) // [1,2,22]  
+
+
+// primitive datatypes are immutable
+
+let myname = "nn"
+
+myname[0] = 'k';
+console.log(myname); // prints bb coz string is immutable
+
+myname = "kk" // this is reassignment hence it works
+console.log(myname)
+
+// structures are mutable
+
+let num = [1,2,3,4]
+
+num[4] = 22
+console.log(num)
+
+
+
